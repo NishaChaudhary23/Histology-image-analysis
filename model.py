f3266431f72c6e1c5b057c11ebdb04a912a355ff
Aakash Rao <mrao.aakash@gmail.com>
2023-01-10 18:54:31 +0530

    idk

--- model.py
+++ model.py
@@ -4,72 +4,71 @@
 #Importing necessary packages
 import pandas as pd
 import tensorflow as tf
-# from tensorflow import keras
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-from tensorflow.contrib.keras.python.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.contrib.keras.python.keras.preprocessing.image import load_img
-from tensorflow.contrib.keras.python.keras.preprocessing.image import img_to_array
-from tensorflow.contrib.keras.python.keras.layers import Conv2D,Dense,Flatten,GlobalAveragePooling2D,MaxPooling2D
-from tensorflow.contrib.keras.python.keras.models import Sequential,Model
-from tensorflow.contrib.keras.python.keras.models import load_model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.layers import Conv2D,Dense,Flatten,GlobalAveragePooling2D,MaxPooling2D
+from tensorflow.keras.models import Sequential,Model
+from tensorflow.keras.models import load_model
 import os
 import cv2
-import tensorflow.contrib.keras.python.keras.backend as K
+import keras.backend as K
 from sklearn.model_selection import train_test_split
-from tensorflow.contrib.keras.python.keras.applications import DenseNet121
-from tensorflow.contrib.keras.python.keras.applications import DenseNet169
-from tensorflow.contrib.keras.python.keras.applications import DenseNet201
-from tensorflow.contrib.keras.python.keras.applications import MobileNetV3Large
-from tensorflow.contrib.keras.python.keras.applications import MobileNetV3Small
-# from tensorflow.contrib.keras.python.keras.applications import ConvNeXtBase
-# from tensorflow.contrib.keras.python.keras.applications import ConvNeXtLarge
-# from tensorflow.contrib.keras.python.keras.applications import ConvNeXtSmall
-# from tensorflow.contrib.keras.python.keras.applications import ConvNeXtTiny
-# from tensorflow.contrib.keras.python.keras.applications import ConvNeXtXLarge
-from tensorflow.contrib.keras.python.keras.applications import EfficientNetB0
-from tensorflow.contrib.keras.python.keras.applications import EfficientNetB1
-from tensorflow.contrib.keras.python.keras.applications import EfficientNetB2
-from tensorflow.contrib.keras.python.keras.applications import EfficientNetB3
-from tensorflow.contrib.keras.python.keras.applications import EfficientNetB4
-from tensorflow.contrib.keras.python.keras.applications import EfficientNetB5
-from tensorflow.contrib.keras.python.keras.applications import EfficientNetB6
-from tensorflow.contrib.keras.python.keras.applications import EfficientNetB7
-# from tensorflow.contrib.keras.python.keras.applications import efficientnet_v2
-# from tensorflow.contrib.keras.python.keras.applications import EfficientNetV2B0
-# from tensorflow.contrib.keras.python.keras.applications import EfficientNetV2B1
-# from tensorflow.contrib.keras.python.keras.applications import EfficientNetV2B2
-# from tensorflow.contrib.keras.python.keras.applications import EfficientNetV2B3
-# from tensorflow.contrib.keras.python.keras.applications import EfficientNetV2L
-# from tensorflow.contrib.keras.python.keras.applications import EfficientNetV2M
-# from tensorflow.contrib.keras.python.keras.applications import EfficientNetV2S
-from tensorflow.contrib.keras.python.keras.applications import InceptionResNetV2
-from tensorflow.contrib.keras.python.keras.applications import InceptionV3
-from tensorflow.contrib.keras.python.keras.applications import MobileNetV2
-from tensorflow.contrib.keras.python.keras.applications import NASNetLarge
-from tensorflow.contrib.keras.python.keras.applications import NASNetMobile
-from tensorflow.contrib.keras.python.keras.applications import ResNet101
-from tensorflow.contrib.keras.python.keras.applications import ResNet101V2
-from tensorflow.contrib.keras.python.keras.applications import ResNet152
-from tensorflow.contrib.keras.python.keras.applications import ResNet152V2
-from tensorflow.contrib.keras.python.keras.applications import ResNet50
-from tensorflow.contrib.keras.python.keras.applications import ResNet50V2
-from tensorflow.contrib.keras.python.keras.applications import VGG16
-from tensorflow.contrib.keras.python.keras.applications import VGG19
-from tensorflow.contrib.keras.python.keras.applications import Xception
-from keras import layers
-from tensorflow.contrib.keras.python.keras.callbacks import Callback, ModelCheckpoint
-from tensorflow.contrib.keras.python.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.contrib.keras.python.keras.layers import Dense, Dropout, Activation, Flatten
-from tensorflow.contrib.keras.python.keras.layers import Conv2D, MaxPooling2D
-from tensorflow.contrib.keras.python.keras.models import Sequential
+from tensorflow.keras.applications import DenseNet121
+from tensorflow.keras.applications import DenseNet169
+from tensorflow.keras.applications import DenseNet201
+from tensorflow.keras.applications import MobileNetV3Large
+from tensorflow.keras.applications import MobileNetV3Small
+# from tensorflow.keras.applications import ConvNeXtBase
+# from tensorflow.keras.applications import ConvNeXtLarge
+# from tensorflow.keras.applications import ConvNeXtSmall
+# from tensorflow.keras.applications import ConvNeXtTiny
+# from tensorflow.keras.applications import ConvNeXtXLarge
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.applications import EfficientNetB1
+from tensorflow.keras.applications import EfficientNetB2
+from tensorflow.keras.applications import EfficientNetB3
+from tensorflow.keras.applications import EfficientNetB4
+from tensorflow.keras.applications import EfficientNetB5
+from tensorflow.keras.applications import EfficientNetB6
+from tensorflow.keras.applications import EfficientNetB7
+# from tensorflow.keras.applications import efficientnet_v2
+# from tensorflow.keras.applications import EfficientNetV2B0
+# from tensorflow.keras.applications import EfficientNetV2B1
+# from tensorflow.keras.applications import EfficientNetV2B2
+# from tensorflow.keras.applications import EfficientNetV2B3
+# from tensorflow.keras.applications import EfficientNetV2L
+# from tensorflow.keras.applications import EfficientNetV2M
+# from tensorflow.keras.applications import EfficientNetV2S
+from tensorflow.keras.applications import InceptionResNetV2
+from tensorflow.keras.applications import InceptionV3
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.applications import NASNetLarge
+from tensorflow.keras.applications import NASNetMobile
+from tensorflow.keras.applications import ResNet101
+from tensorflow.keras.applications import ResNet101V2
+from tensorflow.keras.applications import ResNet152
+from tensorflow.keras.applications import ResNet152V2
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.applications import ResNet50V2
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.applications import VGG19
+from tensorflow.keras.applications import Xception
+from tensorflow.keras import layers
+from tensorflow.keras.callbacks import Callback, ModelCheckpoint
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.models import Sequential
 from sklearn.metrics import classification_report, confusion_matrix
-from tensorflow.contrib.keras.python.keras.optimizers import RMSprop
-from tensorflow.contrib.keras.python.keras.metrics import kl_divergence
-from tensorflow.contrib.keras.python.keras.metrics import mean_squared_error
-from tensorflow.contrib.keras.python.keras.metrics import poisson
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.metrics import kl_divergence
+from tensorflow.keras.metrics import mean_squared_error
+from tensorflow.keras.metrics import poisson
 
 
 
