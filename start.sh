f349788576589343045bb7a26a4e647e407c2eb7
Aakash Rao <mrao.aakash@gmail.com>
2023-01-23 20:17:47 +0530

    Test

--- start.sh
+++ start.sh
@@ -13,4 +13,8 @@ source ~/.bashrc
 
 conda activate histoimgan
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/rintu.kutum/.conda/envs/histoimgan/lib/python3.10/site-packages/nvidia/cudnn/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/rintu.kutum/.conda/envs/histoimgan/lib/python3.10/site-packages/nvidia/cuda_runtime/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/rintu.kutum/.conda/envs/histoimgan/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/rintu.kutum/.conda/pkgs/cudatoolkit-11.2.2-hbe64b41_11/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/rintu.kutum/.conda/pkgs/cudatoolkit-11.0.3-h88f8997_11/lib
 python3 /storage/bic/data/oscc/data/Histology-image-analysis/model.py 
