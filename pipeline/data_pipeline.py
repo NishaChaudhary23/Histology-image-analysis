c93e1395f7bf59a7e22a1fb85cdb7bc7b6262472
Aakash Rao <mrao.aakash@gmail.com>
2023-02-06 14:08:06 +0530

    added file creator

---
+++ pipeline/data_pipeline.py
@@ -0,0 +1,16 @@
+import os
+import numpy as np
+
+def image_generator(path, case, batch_size=32):
+    folders = ['wdoscc', 'mdoscc', 'pdoscc']
+    if case ==1:
+        foldpath_1 = os.path.join(path, folders[0])
+        files_1 = os.listdir(foldpath_1)
+        foldpath_2 = os.path.join(path, folders[1])
+        files_2 = os.listdir(foldpath_2)
+        foldpath_3 = os.path.join(path, folders[2])
+        files_3 = os.listdir(foldpath_3)
+        files = files_1[0.25*len(files_1)] + files_2[0.25*len(files_1)] + files_3[0.5*len(files_1)]
+        print(files)
+
+image_generator("/home/chs.rintu/Documents/chs-lab-ws02/nisha/project-2-oscc/data/original", 1)
