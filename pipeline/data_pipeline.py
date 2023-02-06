213908b94607cdaf38b8ab3927e637717a10e335
Aakash Rao <mrao.aakash@gmail.com>
2023-02-06 14:13:16 +0530

    added file creator

--- pipeline/data_pipeline.py
+++ pipeline/data_pipeline.py
@@ -1,8 +1,11 @@
 import os
 import numpy as np
+import pandas as pd
 
 def image_generator(path, case, batch_size=32):
     folders = ['wdoscc', 'mdoscc', 'pdoscc']
+    train_df = pd.DataFrame(columns=['image', 'label'])
+    test_df = pd.DataFrame(columns=['image', 'label'])
     if case ==1:
         foldpath_1 = os.path.join(path, folders[0])
         files_1 = os.listdir(foldpath_1)
@@ -11,6 +14,12 @@ def image_generator(path, case, batch_size=32):
         foldpath_3 = os.path.join(path, folders[2])
         files_3 = os.listdir(foldpath_3)
         files = files_1[:len(files_1)//25] + files_2[:len(files_2)//25] + files_3[:len(files_3)//50]
-        print(files)
+        for file in files_1[:len(files_1)//25]:
+            train_df = train_df.append({'image': file, 'label': 0}, ignore_index=True)
+        for file in files_2[:len(files_2)//25]:
+            train_df = train_df.append({'image': file, 'label': 0}, ignore_index=True)
+        for file in files_3[:len(files_3)//50]:
+            train_df = train_df.append({'image': file, 'label': 1}, ignore_index=True)
+        print(train_df)
 
 image_generator("/home/chs.rintu/Documents/chs-lab-ws02/nisha/project-2-oscc/data/original", 1)
