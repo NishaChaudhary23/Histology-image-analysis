3ffb9c693e39aee5e1bdfd0ace37358bd3930e8d
Aakash Rao <mrao.aakash@gmail.com>
2023-02-06 14:14:30 +0530

    added file creator

--- pipeline/data_pipeline.py
+++ pipeline/data_pipeline.py
@@ -1,6 +1,8 @@
 import os
 import numpy as np
 import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
 
 def image_generator(path, case, batch_size=32):
     folders = ['wdoscc', 'mdoscc', 'pdoscc']
@@ -15,11 +17,11 @@ def image_generator(path, case, batch_size=32):
         files_3 = os.listdir(foldpath_3)
         files = files_1[:len(files_1)//25] + files_2[:len(files_2)//25] + files_3[:len(files_3)//50]
         for file in files_1[:len(files_1)//25]:
-            train_df = train_df.append({'image': file, 'label': 0}, ignore_index=True)
+            train_df = train_df.append({'image': os.path.join(path, file), 'label': 0}, ignore_index=True)
         for file in files_2[:len(files_2)//25]:
-            train_df = train_df.append({'image': file, 'label': 0}, ignore_index=True)
+            train_df = train_df.append({'image': os.path.join(path, file), 'label': 0}, ignore_index=True)
         for file in files_3[:len(files_3)//50]:
-            train_df = train_df.append({'image': file, 'label': 1}, ignore_index=True)
+            train_df = train_df.append({'image': os.path.join(path, file), 'label': 1}, ignore_index=True)
         print(train_df)
 
 image_generator("/home/chs.rintu/Documents/chs-lab-ws02/nisha/project-2-oscc/data/original", 1)
