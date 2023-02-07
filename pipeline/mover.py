3dd91dcb9b525720bab6032354b07524afdde631
Aakash Rao <mrao.aakash@gmail.com>
2023-02-07 11:51:50 +0530

    added custom loaders

---
+++ pipeline/mover.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+from multiprocessing import Pool
+import multiprocessing as mp
+
+def mover(filename):
+    for root, dirs, files in os.walk(f'home/chs.rintu/Documents/office/researchxoscc/project_2/dataSet/train/{filename}'):
+        for file in files:
+            if file.endswith('.tif'):
+                shutil.copy(os.path.join(root, file), '/home/chs.rintu/Documents/office/researchxoscc/project_2/dataSet/train_all')
+
+
+if __name__ == '__main__':
+    pool = Pool(mp.cpu_count())
+    pool.map(mover, os.listdir('home/chs.rintu/Documents/office/researchxoscc/project_2/dataSet/train'))
