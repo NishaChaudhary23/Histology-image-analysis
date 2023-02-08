f804160c546dcf8843e214eeafeccd16ca7927e7
Aakash Rao <mrao.aakash@gmail.com>
2023-02-08 20:16:36 +0530

    Addded test shell

--- pipeline/data_pipeline.py
+++ pipeline/data_pipeline.py
@@ -214,7 +214,7 @@ def image_generator(case, batch_size=32):
             train_df = train_df.append({'filename': os.path.join(appendpath,file), 'class': "mdoscc"}, ignore_index=True)
         for file in files_3[len(files_3)//2:]:
             train_df = train_df.append({'filename': os.path.join(appendpath,file), 'class': "pdoscc"}, ignore_index=True)
-        train_df.to_csv(os.path.join(outpath1, "master_test.csv"), index=False)
+        train_df.to_csv(os.path.join(outpath7, "master_test.csv"), index=False)
         print(train_df)
 
 
