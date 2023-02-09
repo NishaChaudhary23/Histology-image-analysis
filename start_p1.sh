ebc8ca55aa0e04780efa6390e23c860131ea6afa
Aakash Rao <mrao.aakash@gmail.com>
2023-02-09 15:46:45 +0530

    Added NasNet

--- start_p1.sh
+++ start_p1.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-#PBS -N P1_INV3
+#PBS -N P1_NASL
 #PBS -l host=compute4
-#PBS -o /storage/bic/data/oscc/project_1/model_InceptionV3_out.log
-#PBS -e /storage/bic/data/oscc/project_1/model_InceptionV3_err.log
+#PBS -o /storage/bic/data/oscc/project_1/model_NASNetLarge_out.log
+#PBS -e /storage/bic/data/oscc/project_1/model_NASNetLarge_err.log
 #PBS -q gpu
 
 module load compiler/anaconda3
