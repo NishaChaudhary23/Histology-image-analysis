20a4a6bb190d1617ba7893b4da77bcb510e101e1
Aakash Rao <mrao.aakash@gmail.com>
2023-02-09 14:22:18 +0530

    Addded test shell

--- pipeline/data_pipeline.py
+++ pipeline/data_pipeline.py
@@ -36,6 +36,7 @@ def image_generator(case, batch_size=32):
             train_df = train_df.append({'filename': os.path.join(appendpath,file), 'class': "wmdoscc"}, ignore_index=True)
         for file in files_3[:len(files_3)//2]:
             train_df = train_df.append({'filename': os.path.join(appendpath,file), 'class': "pdoscc"}, ignore_index=True)
+        train_df = train_df.sample(frac=1).reset_index(drop=True)
         train_df.to_csv(os.path.join(outpath1, "train.csv"), index=False)
         print(train_df)
     if case ==2:
@@ -51,7 +52,8 @@ def image_generator(case, batch_size=32):
         for file in files_2[len(files_2)//4:len(files_2)//2]:
             test_df = test_df.append({'filename': os.path.join(appendpath,file), 'class': "wmdoscc"}, ignore_index=True)
         for file in files_3[len(files_3)//2:len(files_3)-len(files_3)//4]:
-            test_df = test_df.append({'filename': os.path.join(appendpath,file), 'class': pdoscc}, ignore_index=True)
+            test_df = test_df.append({'filename': os.path.join(appendpath,file), 'class': "pdoscc"}, ignore_index=True)
+        test_df = test_df.sample(frac=1).reset_index(drop=True)
         test_df.to_csv(os.path.join(outpath1, "test.csv"), index=False)
         print(test_df)
     if case ==7:
@@ -61,10 +63,11 @@ def image_generator(case, batch_size=32):
         foldpath_2 = os.path.join(path, folders[1])
         files_2 = os.listdir(foldpath_2)
         for file in files_1[:len(files_1)//2]:
-            train_df = train_df.append({'filename': os.path.join(appendpath,file), 'class': 0}, ignore_index=True)
+            train_df = train_df.append({'filename': os.path.join(appendpath,file), 'class': "wdoscc"}, ignore_index=True)
         for file in files_2[:len(files_2)//2]:
-            train_df = train_df.append({'filename': os.path.join(appendpath,file), 'class': 1}, ignore_index=True)
+            train_df = train_df.append({'filename': os.path.join(appendpath,file), 'class': "mdoscc"}, ignore_index=True)
         train_df.to_csv(os.path.join(outpath4, "train.csv"), index=False)
+        train_df = train_df.sample(frac=1).reset_index(drop=True)
         print(train_df)
     if case ==13:
         print("Case 13")
@@ -80,6 +83,8 @@ def image_generator(case, batch_size=32):
             train_df = train_df.append({'filename': os.path.join(appendpath,file), 'class': "mdoscc"}, ignore_index=True)
         for file in files_3[len(files_3)//2:]:
             train_df = train_df.append({'filename': os.path.join(appendpath,file), 'class': "pdoscc"}, ignore_index=True)
+        # shuffle the dataframe
+        train_df = train_df.sample(frac=1).reset_index(drop=True)
         train_df.to_csv(os.path.join(outpath7, "master_test.csv"), index=False)
         print(train_df)
 
