31972416461dd1554b4573a354ba8a1da94ab145
Aakash Rao <mrao.aakash@gmail.com>
2023-03-02 14:58:06 +0530

    Added metrics.py

---
+++ pipeline/metrics.py
@@ -0,0 +1,6 @@
+import os
+import pandas as pd
+
+basepath = '/home/chs.rintu/Documents/office/researchxoscc/project_2/output'
+metrics = pd.read_csv(os.path.join(basepath, 'test_pipeline_output.csv'))
+print(metrics.head(5))
