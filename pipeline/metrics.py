772308a9b35ca22a6034dc2aba6f84e6743024a0
Aakash Rao <mrao.aakash@gmail.com>
2023-03-02 15:22:32 +0530

    Added code for roc ccurve

--- pipeline/metrics.py
+++ pipeline/metrics.py
@@ -1,5 +1,8 @@
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, roc_auc_score
+import numpy as np
 
 basepath = '/home/chs.rintu/Documents/office/researchxoscc/project_2/output'
 metrics = pd.read_csv(os.path.join(basepath, 'test_pipeline_output.csv'))
@@ -18,20 +21,5 @@ print(metrics.head(5))
 accuracy = metrics[metrics['prediction'] == metrics['ground_truth']].shape[0] / metrics.shape[0]
 print(f'Accuracy: {accuracy}')
 # calculating the AUC score and curve
-from sklearn.metrics import roc_curve, auc
-import matplotlib.pyplot as plt
-import numpy as np
-fpr, tpr, thresholds = roc_curve(metrics['ground_truth'], metrics['confidence'])
-roc_auc = auc(fpr, tpr)
-print(f'AUC: {roc_auc}')
-plt.figure()
-lw = 2
-plt.plot(fpr, tpr, color='darkorange',  lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
-plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver operating characteristic')
-plt.legend(loc="lower right")
-plt.show()
+fpr1, tpr1, thresholds1 = roc_curve(metrics['ground_truth'], metrics['confidence'])
+auc1 = roc_auc_score(metrics['ground_truth'], metrics['confidence'])
