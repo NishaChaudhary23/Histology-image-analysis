5eb9ccd9a52621b6c60168b2b2bbace4455836fa
Aakash Rao <mrao.aakash@gmail.com>
2023-03-02 16:37:35 +0530

    Added code for roc ccurve

--- pipeline/metrics.py
+++ pipeline/metrics.py
@@ -8,18 +8,18 @@ import numpy as np
 basepath = '/home/chs.rintu/Documents/office/researchxoscc/project_2/output'
 metrics = pd.read_csv(os.path.join(basepath, 'test_pipeline_output.csv'))
 # printing all incorrect predictions
-print(metrics[metrics['ground_truth'] != metrics['final_prediction'] and metrics['model_2a']=='wmdoscc'])
-# print(metrics.head(5))
-# # setting confidence_2a if final_prediction is pdoscc or confidence_2a otherwis
-# metrics_2a = metrics[['model_2a','confidence_2a','ground_truth']]
-# metrics_2b = metrics[['model_2b','confidence_2b','ground_truth']]
+print(metrics[metrics['ground_truth'] != metrics['final_prediction'])
+print(metrics.head(5))
+# setting confidence_2a if final_prediction is pdoscc or confidence_2a otherwis
+metrics_2a = metrics[['model_2a','confidence_2a','ground_truth']]
+metrics_2b = metrics[['model_2b','confidence_2b','ground_truth']]
 
-# # changing ground truth to wmodoscc in metrics_2a if mdoscc or wdoscc is predicted
-# metrics_2a['ground_truth'] = metrics_2a.apply(lambda x: 'wmdoscc' if x['ground_truth'] == 'mdoscc' or x['ground_truth'] == 'wdoscc' else x['ground_truth'], axis=1).astype(str)
-# print (metrics_2a.head(5))
-# # if ground truth doesnt equal to model_2a then confidence_2a is 1-confidence_2a
-# metrics_2a['confidence_2a'] = metrics_2a.apply(lambda x: 1-x['confidence_2a'] if x['ground_truth'] != x['model_2a'] else x['confidence_2a'], axis=1).astype(float)
-# print (metrics_2a.head(5))
+# changing ground truth to wmodoscc in metrics_2a if mdoscc or wdoscc is predicted
+metrics_2a['ground_truth'] = metrics_2a.apply(lambda x: 'wmdoscc' if x['ground_truth'] == 'mdoscc' or x['ground_truth'] == 'wdoscc' else x['ground_truth'], axis=1).astype(str)
+print (metrics_2a.head(5))
+# if ground truth doesnt equal to model_2a then confidence_2a is 1-confidence_2a
+metrics_2a['confidence_2a'] = metrics_2a.apply(lambda x: 1-x['confidence_2a'] if x['ground_truth'] != x['model_2a'] else x['confidence_2a'], axis=1).astype(float)
+print (metrics_2a.head(5))
 
 
 # # Compute ROC curve and ROC area for each class
