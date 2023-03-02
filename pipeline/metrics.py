2e0c98012de2376169ce4d3b14685c41b93784f2
Aakash Rao <mrao.aakash@gmail.com>
2023-03-02 15:00:52 +0530

    Added code for final confidence

--- pipeline/metrics.py
+++ pipeline/metrics.py
@@ -4,3 +4,9 @@ import pandas as pd
 basepath = '/home/chs.rintu/Documents/office/researchxoscc/project_2/output'
 metrics = pd.read_csv(os.path.join(basepath, 'test_pipeline_output.csv'))
 print(metrics.head(5))
+# inserting new column called final confidence to the original dataframe
+metrics.insert(0, 'final_confidence', 0)
+print(metrics.head(5))
+# setting confidence_2a if final_prediction is pdoscc or confidence_2a otherwise
+metrics['final_confidence'] = metrics.apply(lambda x: x['confidence_2a'] if x['final_prediction'] == 'pdoscc' else x['confidence_2b'], axis=1)
+print(metrics.head(5))
