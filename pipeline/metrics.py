28cb766cbfb97c568bd0e9bdce64f2259aba86db
Aakash Rao <mrao.aakash@gmail.com>
2023-03-02 16:19:23 +0530

    Added code for roc ccurve

--- pipeline/metrics.py
+++ pipeline/metrics.py
@@ -18,3 +18,23 @@ print (metrics_2a.head(5))
 # if ground truth doesnt equal to model_2a then confidence_2a is 1-confidence_2a
 metrics_2a['confidence_2a'] = metrics_2a.apply(lambda x: 1-x['confidence_2a'] if x['ground_truth'] != x['model_2a'] else x['confidence_2a'], axis=1).astype(float)
 print (metrics_2a.head(5))
+# calculating roc curve for model_2a
+fpr_2a, tpr_2a, thresholds_2a = roc_curve(metrics_2a['ground_truth'], metrics_2a['confidence_2a'], pos_label='wmdoscc')
+roc_auc_2a = auc(fpr_2a, tpr_2a)
+print (roc_auc_2a)
+# calculating roc curve for model_2b
+fpr_2b, tpr_2b, thresholds_2b = roc_curve(metrics_2b['ground_truth'], metrics_2b['confidence_2b'], pos_label='wmdoscc')
+roc_auc_2b = auc(fpr_2b, tpr_2b)
+print (roc_auc_2b)
+# plottin roc curve for model_2a
+plt.figure()
+lw = 2
+plt.plot(fpr_2a, tpr_2a, color='darkorange',lw=lw, label='ROC curve (area = %0.2f)' % roc_auc_2a)
+plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic example')
+plt.legend(loc="lower right")
+plt.show()
