79de7491084a651a4f9ce47a7ba141ad20a2cb5a
Aakash Rao <mrao.aakash@gmail.com>
2023-03-02 16:15:28 +0530

    Added code for roc ccurve

--- pipeline/metrics.py
+++ pipeline/metrics.py
@@ -12,46 +12,10 @@ print(metrics.head(5))
 metrics.insert(0, 'final_confidence', 0)
 # setting confidence_2a if final_prediction is pdoscc or confidence_2a otherwise
 metrics['final_confidence'] = metrics.apply(lambda x: x['confidence_2a'] if x['final_prediction'] == 'pdoscc' else x['confidence_2b'], axis=1).astype(float)
-# Chosing only final_prediction and final_confidence and ground_truth
-metrics = metrics[['final_prediction', 'final_confidence', 'ground_truth']]
-# renaming the columns
-metrics.columns = ['prediction', 'confidence', 'ground_truth']
-# if prediction is not the same as ground truth then confidence is 1-confidence
-metrics['confidence'] = metrics.apply(lambda x: 1-x['confidence'] if x['prediction'] != x['ground_truth'] else x['confidence'], axis=1).astype(float)
-# encoding the ground_truth column
-metrics['ground_truth'] = metrics['ground_truth'].map({'wdoscc': 0, 'mdoscc': 1, 'pdoscc': 2})
-print(metrics.head(5))
 
-#  calculating the accuracy
-y_true = np.array(metrics['ground_truth'].values)
-y_scores = np.array(metrics['confidence'].values)
+metrics_2a = metrics["model_2a","confidence_2a","ground_truth"]
+metrics_2b = metrics["model_2b","confidence_2b","ground_truth"]
 
-fpr = dict()
-tpr = dict()
-roc_auc = dict()
-for i in range(3):
-    fpr[i], tpr[i], _ = roc_curve(y_true[:]==i, y_scores[:], pos_label=i)
-
-for i in range(3):
-    roc_auc[i] = auc(fpr[i], tpr[i])
-
-print(fpr)
-print(tpr)
-print(roc_auc)
-
-# plot ROC curves for each class and macro-average
-plt.figure()
-lw = 2
-colors = ['red', 'green', 'blue', 'deeppink']
-for i, color in zip(range(3), colors):
-    plt.plot(fpr[i], tpr[i], color=color, lw=lw,
-             label='ROC curve of class {0} (area = {1:0.2f})'
-             ''.format(i, roc_auc[i]))
-plt.plot([0, 1], [0, 1], 'k--', lw=lw)
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver operating characteristic')
-plt.legend(loc="lower right")
-plt.show()
+# changing ground truth to wmodoscc in metrics_2a if mdoscc or wdoscc is predicted
+metrics_2a['ground_truth'] = metrics_2a.apply(lambda x: 'wmodoscc' if x['ground_truth'] == 'mdoscc' or x['ground_truth'] == 'wdoscc' else x['ground_truth'], axis=1).astype(str)
+print (metrics_2a.head(5))
