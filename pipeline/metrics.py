4fedcff9d0db4e89016cd430062e77189e677f2f
Aakash Rao <mrao.aakash@gmail.com>
2023-03-02 16:44:53 +0530

    Added code for roc ccurve

--- pipeline/metrics.py
+++ pipeline/metrics.py
@@ -20,7 +20,7 @@ print (metrics_2a.head(5))
 print(metrics_2a[metrics_2a['ground_truth'] != metrics_2a['model_2a']])
 # filtering out all rows where ground truth is not pdoscc
 
-metrics_2a_0 = metrics_2a[~metrics_2a['ground_truth'] == 'pdoscc']
+metrics_2a_0 = metrics_2a[metrics_2a['ground_truth'] != 'pdoscc']
 # Compute ROC curve and ROC area for each class
 fpr, tpr, _ = roc_curve(metrics_2a_0['model_2a'].apply(lambda x: 1 if x == 'pdoscc' else 0), metrics_2a_0['confidence_2a'], pos_label=0)
 roc_auc = auc(fpr, tpr)
