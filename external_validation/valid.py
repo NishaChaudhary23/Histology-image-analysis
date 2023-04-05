bdf2ee6e97440feb052fe507852fa3d8e52bd2b1
Aakash Rao <mrao.aakash@gmail.com>
2023-04-05 15:06:27 +0530

    added tester

--- external_validation/valid.py
+++ external_validation/valid.py
@@ -22,7 +22,10 @@ os.environ['TF_GPU_ALLOCATOR'] = 'cuda_malloc_async'
 print(tf.__version__)
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 paths = ['/home/chs.rintu/Documents/office/researchxoscc/Ensemble/external_validation_data/images/external validation-P1/normal', '/home/chs.rintu/Documents/office/researchxoscc/Ensemble/external_validation_data/images/external validation-P1/osmf', '/home/chs.rintu/Documents/office/researchxoscc/Ensemble/external_validation_data/images/external validation-P1/oscc']
+datapath = '/home/chs.rintu/Documents/office/researchxoscc/Ensemble/external_validation_data/images/external validation-P1/all'
 master_dataframe = pd.DataFrame()
+
+
 for i in paths:
     items = os.listdir(i)
     # shuffling the list
@@ -30,7 +33,7 @@ for i in paths:
     # taking the first 1000 items
     items = items[:1000]
     # adding the path to the items
-    items = [os.path.join(i, item) for item in items]
+    items = [os.path.join(datapath, item) for item in items]
     # creating a dataframe
     df = pd.DataFrame(items, columns=['filename'])
     # adding the label
@@ -43,4 +46,19 @@ print(master_dataframe)
 master_dataframe = master_dataframe.sample(frac=1).reset_index(drop=True)
 print(master_dataframe)
 
+
+datagen_test = ImageDataGenerator(rescale = 1.0/255.0)
+# Test Data
+test_generator = datagen_test.flow_from_dataframe(
+        dataframe=master_dataframe,
+        folder=datapath,
+        target_size=(300, 300),
+        class_mode='categorical',
+        shuffle=True,
+        validate_filenames=False)
+
+
+# loading the model
+model = load_model('/home/chs.rintu/Documents/office/researchxoscc/project_1/InceptionV3-20230404T121058Z-001/InceptionV3')
+model.summary()
 
