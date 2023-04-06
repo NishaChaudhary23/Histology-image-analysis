26ee675ac29178fe09676cd6e316a59b9618fbf9
Aakash Rao <mrao.aakash@gmail.com>
2023-04-06 15:55:49 +0530

    Added conf matrix

--- external_validation/valid_1.py
+++ external_validation/valid_1.py
@@ -194,7 +194,7 @@ for i in range(len(classes)):
     ax = plt.subplot(2, 3, i+1)
     sns.histplot(x = "prob", data = df_aux, hue = 'class', color = 'b', ax = ax, bins = bins)
     ax.set_title(c)
-    ax.legend([f"Class: {class_keys[c]}", "Rest"], loc = 'upper left')
+    ax.legend([f"Class: {class_keys[c]}", "Rest"], loc = 'upper middle')
     ax.set_xlabel(f"P(x = {class_keys[c]})")
     
     # Calculates the ROC Coordinates and plots the ROC Curves
