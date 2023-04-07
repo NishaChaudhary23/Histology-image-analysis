d75fdc4a1852a5a3fa40735103eb60d40175634f
Aakash Rao <mrao.aakash@gmail.com>
2023-04-07 13:08:59 +0530

    added the ftsne module

--- external_validation/valid_1.py
+++ external_validation/valid_1.py
@@ -136,7 +136,7 @@ test_generator = datagen_test.flow_from_dataframe(
         class_mode='categorical',
         shuffle=True,
         validate_filenames=False,
-        classes=['normal', 'osmf', 'oscc'],)
+        classes=['normal', 'oscc', 'osmf'],)
 
 
 # conf_key = [*test_generator.class_indices.keys()]
