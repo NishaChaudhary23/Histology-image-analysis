5c68c6667d2096607be19ac2ed720d799c5293ed
Aakash Rao <mrao.aakash@gmail.com>
2023-04-07 16:47:53 +0530

    changed file permissions

--- external_validation/valid_2.py
+++ external_validation/valid_2.py
@@ -111,7 +111,7 @@ for i in paths:
     # shuffling the list
     np.random.shuffle(items)
     # taking the first 1000 items
-    items = items[:1000]
+    items = items[:6000]
     print(len(items))
     # adding the path to the items
     items = [os.path.join(datapath, item) for item in items]
@@ -142,136 +142,136 @@ test_generator = datagen_test.flow_from_dataframe(
 conf_key = [*test_generator.class_indices.keys()]
 print(conf_key)
 
-# # loading the model
-# model_2a = load_model('/home/chs.rintu/Documents/office/researchxoscc/Ensemble/models_available/M2a/finetune/M2a_fold_1_finetune.h5')
-# model_2b = load_model('/home/chs.rintu/Documents/office/researchxoscc/Ensemble/models_available/M2b/finetune/M2b_fold_3_finetune.h5')
-# # model.summary()
-# # model_2a.summary()
-# # model_2b.summary()
+# loading the model
+model_2a = load_model('/home/chs.rintu/Documents/office/researchxoscc/Ensemble/models_available/M2a/finetune/M2a_fold_1_finetune.h5')
+model_2b = load_model('/home/chs.rintu/Documents/office/researchxoscc/Ensemble/models_available/M2b/finetune/M2b_fold_3_finetune.h5')
+# model.summary()
+# model_2a.summary()
+# model_2b.summary()
 
-# eval_2a = model_2a.predict(test_generator)
-# eval_2b = model_2b.predict(test_generator)
-# # finding the class with the highest probability
-# scores_2a = eval_2a
-# scores_2b = eval_2b
-# scores_2b = eval_2a[:,1]*eval_2b[:]
+eval_2a = model_2a.predict(test_generator)
+eval_2b = model_2b.predict(test_generator)
+# finding the class with the highest probability
+scores_2a = eval_2a
+scores_2b = eval_2b
+scores_2b = eval_2a[:,1]*eval_2b[:]
 
-# # computing scaled down scores
-# scores_2a = np.array(scores_2a)
-# scores_2a_1 = scores_2a[:,0]
-# scores_2a_2 = scores_2a[:,1]
-# scores_2b = np.array(scores_2b)
-# scores_2b_mult = np.array([scores_2b[i]*scores_2a_2[i] for i in range(len(scores_2a_2))])
-# pred_array = np.array([])
-# for i in range(len(scores_2a_1)):
-#     ai = np.hstack((scores_2a_1[i], scores_2b_mult[i]))
-#     pred_array = np.vstack((pred_array, ai)) if pred_array.size else ai
-# scores = pred_array
+# computing scaled down scores
+scores_2a = np.array(scores_2a)
+scores_2a_1 = scores_2a[:,0]
+scores_2a_2 = scores_2a[:,1]
+scores_2b = np.array(scores_2b)
+scores_2b_mult = np.array([scores_2b[i]*scores_2a_2[i] for i in range(len(scores_2a_2))])
+pred_array = np.array([])
+for i in range(len(scores_2a_1)):
+    ai = np.hstack((scores_2a_1[i], scores_2b_mult[i]))
+    pred_array = np.vstack((pred_array, ai)) if pred_array.size else ai
+scores = pred_array
 
-# eval = np.argmax(eval, axis=1)
-# # converting the class to the original label
-# eval = [conf_key[i] for i in eval]
-# gt = np.array(test_generator.classes)
+eval = np.argmax(eval, axis=1)
+# converting the class to the original label
+eval = [conf_key[i] for i in eval]
+gt = np.array(test_generator.classes)
+gt = [conf_key[i] for i in gt]
+conf = confusion_matrix(gt, eval)
+# conf = pd.DataFrame(conf, columns=conf_key, index=conf_key)
+
+# conf = conf.values[:,1:]
+conf = conf.astype(np.int32)
+conf_percentages = conf / conf.sum(axis=1)[:, np.newaxis]
+conf_percentages = conf_percentages * 100
+conf_percentages = np.round(conf_percentages, 2).flatten()
+labels = [f"{v1}\n{v2}%" for v1, v2 in
+        zip(conf.flatten(),conf_percentages)]
+labels = np.asarray(labels).reshape(3,3)
+plt.figure(figsize=(3.5,3))
+sns.heatmap(conf_percentages.reshape((3,3)), annot=labels, xticklabels=conf_key, cmap=sns.color_palette("ch:s=-.2,r=.6", as_cmap=True), yticklabels=conf_key, fmt='', cbar=True, annot_kws={"font":'Sans',"size": 9.5,"fontstyle":'italic' })
+plt.xlabel('Predicted',fontname="Sans", fontsize=9, labelpad=10,fontweight='bold')
+plt.ylabel('Ground Truth',fontname="Sans", fontsize=9, labelpad=10,fontweight='bold')
+plt.title(f'External Validation Confusion Matrix',fontname="Sans", fontsize=11,fontweight='bold')
+plt.tight_layout()
+plt.savefig(f'{plotpath}project_1_exVal_cm.png', dpi = 300)
+
+gt = np.array(test_generator.classes)
+score_gt = np.array([scores[i][gt[i]] for i in range(len(gt))])
 # gt = [conf_key[i] for i in gt]
-# conf = confusion_matrix(gt, eval)
-# # conf = pd.DataFrame(conf, columns=conf_key, index=conf_key)
 
-# # conf = conf.values[:,1:]
-# conf = conf.astype(np.int32)
-# conf_percentages = conf / conf.sum(axis=1)[:, np.newaxis]
-# conf_percentages = conf_percentages * 100
-# conf_percentages = np.round(conf_percentages, 2).flatten()
-# labels = [f"{v1}\n{v2}%" for v1, v2 in
-#         zip(conf.flatten(),conf_percentages)]
-# labels = np.asarray(labels).reshape(3,3)
-# plt.figure(figsize=(3.5,3))
-# sns.heatmap(conf_percentages.reshape((3,3)), annot=labels, xticklabels=conf_key, cmap=sns.color_palette("ch:s=-.2,r=.6", as_cmap=True), yticklabels=conf_key, fmt='', cbar=True, annot_kws={"font":'Sans',"size": 9.5,"fontstyle":'italic' })
-# plt.xlabel('Predicted',fontname="Sans", fontsize=9, labelpad=10,fontweight='bold')
-# plt.ylabel('Ground Truth',fontname="Sans", fontsize=9, labelpad=10,fontweight='bold')
-# plt.title(f'External Validation Confusion Matrix',fontname="Sans", fontsize=11,fontweight='bold')
-# plt.tight_layout()
-# plt.savefig(f'{plotpath}project_1_exVal_cm.png', dpi = 300)
+X_test = pd.DataFrame(data={'class':gt, 'prob':score_gt})
 
-# gt = np.array(test_generator.classes)
-# score_gt = np.array([scores[i][gt[i]] for i in range(len(gt))])
-# # gt = [conf_key[i] for i in gt]
-
-# X_test = pd.DataFrame(data={'class':gt, 'prob':score_gt})
-
-# bins = [i/20 for i in range(20)] + [1]
-# classes = [0, 1, 2]
-# roc_auc_ovr = {}
-# for i in range(len(classes)):
-#     # Gets the class
-#     c = classes[i]
+bins = [i/20 for i in range(20)] + [1]
+classes = [0, 1, 2]
+roc_auc_ovr = {}
+for i in range(len(classes)):
+    # Gets the class
+    c = classes[i]
     
-#     # Prepares an auxiliar dataframe to help with the plots
-#     df_aux = X_test.copy()
-#     df_aux['class'] = df_aux['class'].apply(lambda x: 1 if x == c else 0)
-#     df_aux = df_aux.reset_index(drop = True)
+    # Prepares an auxiliar dataframe to help with the plots
+    df_aux = X_test.copy()
+    df_aux['class'] = df_aux['class'].apply(lambda x: 1 if x == c else 0)
+    df_aux = df_aux.reset_index(drop = True)
     
-#     # Plots the probability distribution for the class and the rest
-#     try:
-#         plt.figure(figsize = (3.5,3))
-#         ax = plt.subplot(1,1,1)
-#         sns.histplot(x = "prob", data = df_aux, hue = 'class', color = 'b', ax = ax, bins = bins)
-#         ax.set_title(f'Probability Distribution for {conf_key[c]}')
-#         ax.legend([f"Class: {conf_key[c]}", "Rest"], loc = 'upper center')
-#         ax.set_xlabel(f"P(x = {conf_key[c]})")
-#         # Calculates the ROC Coordinates and plots the ROC Curves
-#         plt.tight_layout()  
-#         plt.savefig(f'{plotpath}project_1_exVal_probability_distribution_{conf_key[c]}.png', dpi = 300)
-#     except:
-#         print(f"Error in Probability Distribution for {conf_key[c]}")
-#         pass
+    # Plots the probability distribution for the class and the rest
+    try:
+        plt.figure(figsize = (3.5,3))
+        ax = plt.subplot(1,1,1)
+        sns.histplot(x = "prob", data = df_aux, hue = 'class', color = 'b', ax = ax, bins = bins)
+        ax.set_title(f'Probability Distribution for {conf_key[c]}')
+        ax.legend([f"Class: {conf_key[c]}", "Rest"], loc = 'upper center')
+        ax.set_xlabel(f"P(x = {conf_key[c]})")
+        # Calculates the ROC Coordinates and plots the ROC Curves
+        plt.tight_layout()  
+        plt.savefig(f'{plotpath}project_1_exVal_probability_distribution_{conf_key[c]}.png', dpi = 300)
+    except:
+        print(f"Error in Probability Distribution for {conf_key[c]}")
+        pass
 
-#     try:
-#         plt.figure(figsize = (3.5,3))
-#         ax_bottom = plt.subplot(1, 1, 1)
-#         tpr, fpr = get_all_roc_coordinates(df_aux['class'], df_aux['prob'])
-#         plot_roc_curve(tpr, fpr, scatter = False, ax = ax_bottom, conf_key = ['normal', 'osmf', 'oscc'])
-#         ax_bottom.set_title(f'ROC Curve OvR for {conf_key[c]}')
-#         plt.tight_layout()  
-#         plt.savefig(f'{plotpath}project_1_exVal_roc_ovr_curve_{conf_key[c]}.png', dpi = 300)
-#     except:
-#         print(f"Error in ROC Curve for {conf_key[c]}")
-#         pass
-#     # Calculates the ROC AUC OvR
-#     roc_auc_ovr[c] = roc_auc_score(df_aux['class'], df_aux['prob'], multi_class = 'ovr')
+    try:
+        plt.figure(figsize = (3.5,3))
+        ax_bottom = plt.subplot(1, 1, 1)
+        tpr, fpr = get_all_roc_coordinates(df_aux['class'], df_aux['prob'])
+        plot_roc_curve(tpr, fpr, scatter = False, ax = ax_bottom, conf_key = ['normal', 'osmf', 'oscc'])
+        ax_bottom.set_title(f'ROC Curve OvR for {conf_key[c]}')
+        plt.tight_layout()  
+        plt.savefig(f'{plotpath}project_1_exVal_roc_ovr_curve_{conf_key[c]}.png', dpi = 300)
+    except:
+        print(f"Error in ROC Curve for {conf_key[c]}")
+        pass
+    # Calculates the ROC AUC OvR
+    roc_auc_ovr[c] = roc_auc_score(df_aux['class'], df_aux['prob'], multi_class = 'ovr')
 
-# print("Individual Figures Plot Complete")
+print("Individual Figures Plot Complete")
 
-# plt.figure(figsize = (12, 8))
-# bins = [i/20 for i in range(20)] + [1]
-# classes = [0, 1, 2]
-# roc_auc_ovr = {}
-# try:
-#     for i in range(len(classes)):
-#         # Gets the class
-#         c = classes[i]
+plt.figure(figsize = (12, 8))
+bins = [i/20 for i in range(20)] + [1]
+classes = [0, 1, 2]
+roc_auc_ovr = {}
+try:
+    for i in range(len(classes)):
+        # Gets the class
+        c = classes[i]
         
-#         # Prepares an auxiliar dataframe to help with the plots
-#         df_aux = X_test.copy()
-#         df_aux['class'] = df_aux['class'].apply(lambda x: 1 if x == c else 0)
-#         df_aux = df_aux.reset_index(drop = True)
+        # Prepares an auxiliar dataframe to help with the plots
+        df_aux = X_test.copy()
+        df_aux['class'] = df_aux['class'].apply(lambda x: 1 if x == c else 0)
+        df_aux = df_aux.reset_index(drop = True)
         
-#         # Plots the probability distribution for the class and the rest
-#         ax = plt.subplot(2, 3, i+1)
-#         sns.histplot(x = "prob", data = df_aux, hue = 'class', color = 'b', ax = ax, bins = bins)
-#         ax.set_title(f'Figures for {conf_key[c]}')
-#         ax.legend([f"Class: {conf_key[c]}", "Rest"], loc = 'upper center')
-#         ax.set_xlabel(f"P(x = {conf_key[c]})")
-#         # Calculates the ROC Coordinates and plots the ROC Curves
-#         ax_bottom = plt.subplot(2, 3, i+4)
-#         tpr, fpr = get_all_roc_coordinates(df_aux['class'], df_aux['prob'])
-#         plot_roc_curve(tpr, fpr, scatter = False, ax = ax_bottom, conf_key = ['normal', 'osmf', 'oscc'])
-#         ax_bottom.set_title("ROC Curve OvR")
+        # Plots the probability distribution for the class and the rest
+        ax = plt.subplot(2, 3, i+1)
+        sns.histplot(x = "prob", data = df_aux, hue = 'class', color = 'b', ax = ax, bins = bins)
+        ax.set_title(f'Figures for {conf_key[c]}')
+        ax.legend([f"Class: {conf_key[c]}", "Rest"], loc = 'upper center')
+        ax.set_xlabel(f"P(x = {conf_key[c]})")
+        # Calculates the ROC Coordinates and plots the ROC Curves
+        ax_bottom = plt.subplot(2, 3, i+4)
+        tpr, fpr = get_all_roc_coordinates(df_aux['class'], df_aux['prob'])
+        plot_roc_curve(tpr, fpr, scatter = False, ax = ax_bottom, conf_key = ['normal', 'osmf', 'oscc'])
+        ax_bottom.set_title("ROC Curve OvR")
         
-#         # Calculates the ROC AUC OvR
-#         roc_auc_ovr[c] = roc_auc_score(df_aux['class'], df_aux['prob'], multi_class = 'ovr')
-#     plt.tight_layout()
-#     plt.savefig(f'{plotpath}project_1_exVal_roc.png', dpi = 300)
-#     print("Composite Figure Plot Complete")
-# except:
-#     print("Error in Composite Figure Plot")
-#     pass
+        # Calculates the ROC AUC OvR
+        roc_auc_ovr[c] = roc_auc_score(df_aux['class'], df_aux['prob'], multi_class = 'ovr')
+    plt.tight_layout()
+    plt.savefig(f'{plotpath}project_1_exVal_roc.png', dpi = 300)
+    print("Composite Figure Plot Complete")
+except:
+    print("Error in Composite Figure Plot")
+    pass
