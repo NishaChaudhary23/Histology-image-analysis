5a3fd10a10fc9c115ded5483872103ca818482c7
Aakash Rao <mrao.aakash@gmail.com>
2023-04-13 15:42:40 +0530

    added new file

--- class_balance/classes.py
+++ class_balance/classes.py
@@ -5,9 +5,32 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-for folder in os.listdir('/home/chs.rintu/Documents/office/researchxoscc/Ensemble/external_validation_data/images/external validation-P1/'):
-    if folder == 'all':
-        continue
-    print(folder)
-    print(len(os.listdir(os.path.join('/home/chs.rintu/Documents/office/researchxoscc/Ensemble/external_validation_data/images/external validation-P1/', folder))))
-    
+paths = ['/home/chs.rintu/Documents/office/researchxoscc/Ensemble/external_validation_data/images/external validation-P1/normal', '/home/chs.rintu/Documents/office/researchxoscc/Ensemble/external_validation_data/images/external validation-P1/osmf', '/home/chs.rintu/Documents/office/researchxoscc/Ensemble/external_validation_data/images/external validation-P1/oscc']
+datapath = '/home/chs.rintu/Documents/office/researchxoscc/Ensemble/external_validation_data/images/external validation-P1/all'
+plotpath = '/home/chs.rintu/Documents/office/researchxoscc/Ensemble/plots/project_1/'
+
+if not os.path.exists(datapath):
+    os.makedirs(datapath)
+
+master_dataframe = pd.DataFrame()
+
+for i in paths:
+    items = os.listdir(i)
+    # shuffling the list
+    np.random.shuffle(items)
+    # taking the first 1000 items
+    items = items[:1000]
+    print(len(items))
+    # adding the path to the items
+    items = [os.path.join(datapath, item) for item in items]
+    # creating a dataframe
+    df = pd.DataFrame(items, columns=['filename'])
+    # adding the label
+    df['class'] = i.split('/')[-1]
+    # appending the dataframe to the master dataframe
+    master_dataframe = master_dataframe.append(df)
+
+# print(master_dataframe)
+# shuffling the master dataframe
+# master_dataframe = master_dataframe.sample(frac=1).reset_index(drop=True)
+print(master_dataframe.head())
