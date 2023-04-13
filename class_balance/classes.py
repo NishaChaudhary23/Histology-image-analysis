de94c4c6fdcc93dedf6dcccdba1213b3c290b3d9
Aakash Rao <mrao.aakash@gmail.com>
2023-04-13 15:48:11 +0530

    added new file

--- class_balance/classes.py
+++ class_balance/classes.py
@@ -13,28 +13,31 @@ outpath = '/home/chs.rintu/Documents/office/researchxoscc/Ensemble/external_vali
 if not os.path.exists(datapath):
     os.makedirs(datapath)
 
-master_dataframe = pd.DataFrame()
+# master_dataframe = pd.DataFrame()
 
-for i in paths:
-    items = os.listdir(i)
-    # shuffling the list
-    np.random.shuffle(items)
-    # taking the first 1000 items
-    items = items[:1000]
-    print(len(items))
-    # adding the path to the items
-    items = [os.path.join(datapath, item) for item in items]
-    # creating a dataframe
-    df = pd.DataFrame(items, columns=['filename'])
-    # adding the label
-    df['class'] = i.split('/')[-1]
-    # appending the dataframe to the master dataframe
-    master_dataframe = master_dataframe.append(df)
+# for i in paths:
+#     items = os.listdir(i)
+#     # shuffling the list
+#     np.random.shuffle(items)
+#     # taking the first 1000 items
+#     items = items[:1000]
+#     print(len(items))
+#     # adding the path to the items
+#     items = [os.path.join(datapath, item) for item in items]
+#     # creating a dataframe
+#     df = pd.DataFrame(items, columns=['filename'])
+#     # adding the label
+#     df['class'] = i.split('/')[-1]
+#     # appending the dataframe to the master dataframe
+#     master_dataframe = master_dataframe.append(df)
 
-# print(master_dataframe)
-# shuffling the master dataframe
-# master_dataframe = master_dataframe.sample(frac=1).reset_index(drop=True)
+# # print(master_dataframe)
+# # shuffling the master dataframe
+# # master_dataframe = master_dataframe.sample(frac=1).reset_index(drop=True)
+# print(master_dataframe.head())
+
+# # saving the master dataframe
+# master_dataframe.to_csv(os.path.join(outpath, 'master_dataframe.csv'), index=False)
+
+master_dataframe = pd.read_csv(os.path.join(outpath, 'master_dataframe.csv'))
 print(master_dataframe.head())
-
-# saving the master dataframe
-master_dataframe.to_csv(os.path.join(outpath, 'master_dataframe.csv'), index=False)
