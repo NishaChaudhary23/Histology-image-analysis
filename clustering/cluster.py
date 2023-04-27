4d3b9f50c53770ddb53b1233b6817809390d4ebe
Aakash Rao <mrao.aakash@gmail.com>
2023-04-27 15:46:26 +0530

    added list creator

--- clustering/cluster.py
+++ clustering/cluster.py
@@ -29,3 +29,25 @@ for i in range(len(master_dataframe)):
 
 print(len(images))
 print(len(labels))
+
+# converting images and labels to numpy array
+images = np.array(images)
+labels = np.array(labels)
+
+# beginning FTSNE
+# reshaping images to 1D array
+images = images.reshape(len(images), -1)
+print(images.shape)
+
+# creating a dataframe with images and labels
+df = pd.DataFrame(images)
+df['labels'] = labels
+print(df.head())
+
+model = TSNE(n_components=2, random_state=0, perplexity=50, n_iter=5000)
+tsne_data = model.fit_transform(df.iloc[:, :-1])
+
+# creating a new dataframe which help us in ploting the result data
+tsne_data = np.vstack((tsne_data.T, df['labels'])).T
+tsne_df = pd.DataFrame(data=tsne_data, columns=('Dim_1', 'Dim_2', 'labels'))
+tsne_df.to_csv(os.path.join(outpath, 'tsne_data.csv'), index=False)
