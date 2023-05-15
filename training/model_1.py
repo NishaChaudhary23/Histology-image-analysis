ac0e524933998b61798d22a026efcdc107e8f10a
Aakash Rao <mrao.aakash@gmail.com>
2023-05-15 13:13:24 +0530

    added new parameters for the model

--- training/model_1.py
+++ training/model_1.py
@@ -474,7 +474,8 @@ if model_type == 'InceptionV3':
 	for layer in inception.layers:
 			layer.trainable = True
 	x = layers.Flatten()(inception.output)
-	x = layers.Dropout(0.2)(x)
+	x = layers.Dropout(0.5)(x)
+	x = layers.Dense(10240, activation = 'relu', kernel_regularizer=l2(0.01))(x)
 	x = layers.Dense(1024, activation = 'relu', kernel_regularizer=l2(0.01))(x)
 	x = layers.Dense(3, activation = 'softmax', kernel_regularizer=l2(0.01))(x)
 	model = Model(inception.input, x)
