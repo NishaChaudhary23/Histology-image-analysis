697e6bca07228c5dc9abb32ca612369b66892685
Aakash Rao <mrao.aakash@gmail.com>
2023-06-17 20:43:32 +0530

    new crap

--- training/shell.sh
+++ training/shell.sh
@@ -1,4 +1,5 @@
 conda activate histoimgan
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/chs.rintu/anaconda3/anaconda3/envs/histoimgan/lib/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/chs.rintu/anaconda3/anaconda3/envs/histoimgan/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/anaconda3/pkgs/cudatoolkit-11.2.2-hbe64b41_11/lib
 python model.py
 # libcudart.so
